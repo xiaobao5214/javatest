7e3ef7410b559177698707391e93d627f4977b4a
jingbao <jingbao@didapinche.com>
2017-07-14 14:35:58 +0800

    添加hello

--- src/First.java
+++ src/First.java
@@ -5,5 +5,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
+        System.out.println("hello");
     }
 }
