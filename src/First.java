5b57783d9934d5a17047aef8f814ce979eb5b5de
xiaobao5214 <xiaobao5214@163.com>
2017-07-14 14:45:01 +0800

    Update First.java

--- src/First.java
+++ src/First.java
@@ -5,6 +5,5 @@ public class First {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
-        System.out.println("hello");
     }
 }
