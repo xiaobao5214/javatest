669c78fd5886d3e972b96514d68d2c49cbdee094
jingbao <jingbao@didapinche.com>
2017-08-01 09:17:14 +0800

    set get

--- src/aa.java
+++ src/aa.java
@@ -2,9 +2,17 @@
  * Created by jingbao on 2017/7/14.
  */
 public class aa {
-  private String name;
-  private int age;
-  public String getName(){
-    return name;
-  }
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        System.out.println("12333");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
 }
