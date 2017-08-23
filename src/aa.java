fac401d0446d0c94e97987561941a4593f47e23b
jingbao <jingbao@didapinche.com>
2017-08-23 13:08:05 +0800

    56616154sdafa

--- src/aa.java
+++ src/aa.java
@@ -6,7 +6,7 @@ public class aa {
     private int age;
 
     public static void main(String[] args) {
-        System.out.println("545434");
+        System.out.println("545434ssssssssssss");
     }
 
     public void setName(String name) {
