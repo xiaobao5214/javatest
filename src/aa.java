f41bfed90b272f62f5c2d3cba58f387134425d53
jingbao <jingbao@didapinche.com>
2017-09-29 09:54:24 +0800

    试试cherry-pick命令
    (cherry picked from commit 551dcd8)

--- src/aa.java
+++ src/aa.java
@@ -2,4 +2,17 @@
  * Created by jingbao on 2017/7/14.
  */
 public class aa {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        System.out.println("我来试试cherry-pick命令");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
 }
